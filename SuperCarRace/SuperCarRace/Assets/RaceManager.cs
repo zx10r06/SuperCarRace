992919bfa19550420a579c5a7479a0250a0183a7
zx10r06 <zx10r06@live.com.au>
2018-04-08 21:30:37 +1000

    Set observed component

--- SuperCarRace/SuperCarRace/Assets/RaceManager.cs
+++ SuperCarRace/SuperCarRace/Assets/RaceManager.cs
@@ -17,7 +17,7 @@ public class RaceManager : MonoBehaviour {
     Canvas MultiplayerCanvas;
 
     int raceId = 0;
-    public int selectedCarIndex = 0;
+    public int selectedCarIndex = 1;
 
     string playerCarPrefabName { get;  set; }
 
